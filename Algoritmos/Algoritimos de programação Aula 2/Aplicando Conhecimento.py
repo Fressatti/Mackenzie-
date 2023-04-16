95a7acbd8142d531d24c9b43a694b6b3adab777e
Caio Fressatti <Caio.fressatti@gmail.com>
2023-04-16 18:53:51 -0300

    Att

--- Algoritmos/Algoritimos de programação Aula 2/Aplicando Conhecimento.py
+++ Algoritmos/Algoritimos de programação Aula 2/Aplicando Conhecimento.py
@@ -7,8 +7,8 @@ Min = (Custo / Preco)
 Lucro = Custo * 0.23
 Qtd_lucro = (Custo + Lucro) / Preco
 
-rounded_Lucro = math.ceil(Min)
-rounded_Qtd_lucro = math.ceil(Qtd_lucro)
+Arredonda_Lucro = math.ceil(Min)
+Arredonda_Qtd_lucro = math.ceil(Qtd_lucro)
 
-print( rounded_Lucro)
-print( rounded_Qtd_lucro)
+print( Arredonda_Lucro)
+print( Arredonda_Qtd_lucro)
